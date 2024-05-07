3452aaae51a44ca57f8a44c5f99e150fa0ff7bdf
mayados <mariannedos.cours@gmail.com>
2024-05-07 14:24:16 +0200

    add Lists and for loops

--- Number split - Odd and Even/Program.cs
+++ Number split - Odd and Even/Program.cs
@@ -10,6 +10,38 @@ namespace Number_split___Odd_and_Even
     {
         static void Main(string[] args)
         {
+
+            List<int> listEven = new List<int>();
+            List<int> listOdd = new List<int>();
+
+            for(int i = 1; i <= 20; i++)
+            {
+                if(i%2 == 0)
+                {
+                    listEven.Add(i);
+                }
+                else
+                {
+                    listOdd.Add(i);
+                }
+            }
+            
+            Console.WriteLine("The content of the even list is :");
+
+            for(int i = 0; i < listEven.Count; i++)
+            {
+                Console.Write($"{listEven[i]} "); ;
+            }
+
+            Console.WriteLine("\nThe content of the odd list is :");
+
+            for (int i = 0; i < listOdd.Count; i++)
+            {
+                Console.Write($"{listOdd[i]} "); ;
+            }
+            Console.ReadLine();
+
         }
+
     }
 }
